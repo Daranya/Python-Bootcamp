792fd435ed991836bdb3682d3ab3415aa4820050
Daranya Maniseharan <65844372+Daranya@users.noreply.github.com>
2025-04-02 22:40:09 +0200

    Day 2

---
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -0,0 +1,8 @@
+len("12345")
+
+print(type("Hello!"))
+print(type(1234567890))
+print(type(2.473543333))
+print(type(True))
+
+print("Number of letters in your name: " + str(len(input("Enter your name"))))
